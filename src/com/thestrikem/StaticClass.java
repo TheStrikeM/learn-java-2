2821277f3ef97cece9827be98a0fb9539eb59ccc
TheStrikeM <thestrikem@gmail.com>
2021-08-15 20:33:36 +0500

    Learn

--- src/com/thestrikem/StaticClass.java
+++ src/com/thestrikem/StaticClass.java
@@ -1,5 +1,12 @@
 package com.thestrikem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 enum CarEnum {
     SMALL("Генадий"), BIG("Валерий");
     private String name;
@@ -41,3 +48,89 @@ class ExampleClass {
         System.out.println(CarEnum.BIG.getName());
     }
 }
+
+class ExampleClass2 {
+    private String[] nameList;
+
+    ExampleClass2(String[] nameList) {
+        this.nameList = nameList;
+    }
+
+    boolean isItemInList(String findValue) {
+        boolean result = false;
+        for (String name : nameList) {
+            if (name.equals(findValue)) { result = true; break; }
+        }
+        return result;
+    }
+
+    int getIndexOnList(String findValue) {
+        boolean result = false;
+        int index = 0;
+        for (int name = 0; name < nameList.length; name++) {
+            if (nameList[name].equals(findValue)) { result = true; index = name; break; }
+        }
+        if (result) { return index; }
+        else { return -1; }
+    }
+
+    String getItemOnList(int index) {
+        return nameList[index];
+    }
+
+    int parseToInt(String value) {
+        int[] numbersList = {0,1,2,3,4,5,6,7,8,9};
+        return Integer.parseInt(value);
+    }
+
+    int parseToIntWithValueOf(String value) {
+        return Integer.valueOf(value);
+    }
+
+    public String[] getNameList() {
+        return nameList;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ExampleClass2 exampleClass2 = new ExampleClass2(new String[]{"Генадий", "Василий", "Петр"});
+        int result = 0;
+        String findValue = "Петр";
+
+        if (exampleClass2.isItemInList(findValue)) {
+            result = exampleClass2.getIndexOnList(findValue);
+            System.out.println(exampleClass2.getNameList());
+        }
+        else {
+            System.out.println(false);
+        }
+
+        System.out.println(exampleClass2.parseToInt("576"));
+
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+
+        for (final int item : list) {
+            System.out.println(item);
+        }
+
+        System.out.println(list);
+
+        String item = "test";
+        String item2 = "test";
+        if (item == item2) {
+            System.out.println("True 1");
+        }
+        if (item.equals(item2)) {
+            System.out.println("True 2");
+        }
+
+        System.out.println(new Date());
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String name = bufferedReader.readLine();
+        boolean resultS = name == "Генадий" || name == "Василий";
+        System.out.println("Your name is " + name + " " + resultS);
+    }
+}
+
